943b1b3e130610e3e0e5c4d9d766a0c8fe737679
Arman Ali <armaanali5601@gmail.com>
2024-11-19 00:37:16 +0530

    Card component added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Card() {
-  return (
-    <div>
-      
-    </div>
+function Card({id,name,image,price}) {
+    return (
+        <div>
+            {/* Product Cards */}
+            <div key={id} className="border border-gray-300 rounded-lg p-2.5 max-w-[700px] max-h-[500px] text-center">
+                <img className="w-56 h-52" src={image} alt={name} />
+                <h2>{name}</h2>
+                <p className="pb-2 font-bold">Price: ${price}</p>
+
+                <Link to={`/product/${id}`} className="bg-cyan-500 rounded-lg p-1 text-white hover:bg-gray-400 mb">
+                    View Details
+                </Link>
+            </div>
+        </div>     
   )
 }
 
